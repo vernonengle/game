1a6d72b67439a7f26c2591ceb4e5eaaaee54ff4e
vengle <vernon.g.engle@accenture.com>
2019-07-19 10:48:37 +1000

    added test dependencies

---
+++ WitsAndWagersFunction/src/test/java/witsandwagers/GrandMasterSetStageTest.java
@@ -0,0 +1,61 @@
+package witsandwagers;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dynamodb.DynamoDBClientUtil;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import pojo.GatewayResponse;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DynamoDBClientUtil.class})
+public class GrandMasterSetStageTest {
+
+    GrandMasterSetStage grandMasterSetStage = new GrandMasterSetStage();
+
+    @Rule
+    public static final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+
+    static DynamoDB dynamoDB;
+
+    Context context = PowerMockito.mock(Context.class);
+
+    @BeforeClass
+    public static void setup() {
+        PowerMockito.mockStatic(DynamoDBClientUtil.class);
+        dynamoDB = PowerMockito.mock(DynamoDB.class);
+        PowerMockito.when(DynamoDBClientUtil.getDynamoDBClient(Regions.AP_SOUTHEAST_1)).thenReturn(dynamoDB);
+        environmentVariables.set("STAGE_TABLE_NAME","STAGE_TABLE_DB");
+    }
+
+    @Test
+    public void handleRequest() throws IOException {
+        LinkedHashMap<String, Object> input = new LinkedHashMap<>();
+        LinkedHashMap<String, Object> queryStringParameters = new LinkedHashMap<>();
+        queryStringParameters.put("id", "1");
+        input.put("queryStringParameters",queryStringParameters);
+
+        GatewayResponse gatewayResponse = grandMasterSetStage.handleRequest(input, context);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> map = mapper.readValue(gatewayResponse.getBody(), Map.class);
+        assertThat(map, notNullValue());
+        assertThat(map.get("id"), equalTo("1"));
+    }
+}

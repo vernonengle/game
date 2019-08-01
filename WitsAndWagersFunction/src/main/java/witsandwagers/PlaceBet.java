8fcb2db074146daecf252e60a62d546823558db7
vinlly.mae.sta.maria <vinllymae@gmail.com>
2019-08-01 11:31:56 +0800

    add stage check

--- WitsAndWagersFunction/src/main/java/witsandwagers/PlaceBet.java
+++ WitsAndWagersFunction/src/main/java/witsandwagers/PlaceBet.java
@@ -1,14 +1,7 @@
 package witsandwagers;
 
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.document.AttributeUpdate;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.ItemCollection;
-import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
-import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
-import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
-import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
 import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
@@ -25,53 +18,59 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-
 public class PlaceBet implements RequestHandler<LinkedHashMap<String, Object>, GatewayResponse> {
 
     public GatewayResponse handleRequest(final LinkedHashMap<String, Object> input, final Context context) {
         LambdaLogger logger = context.getLogger();
         Map<String, String> headers = ResponseUtil.getHeaderMap("application/json");
         DynamoDB dynamoDB = DynamoDBClientUtil.getDynamoDBClient(Regions.AP_SOUTHEAST_1);
+        String Stage_TableName = System.getenv("STAGE_TABLE_NAME");
+        Table table1 = dynamoDB.getTable(Stage_TableName);
+        Item item1 = table1.getItem("id", "PlaceBetStage");
 
-        logger.log("Querying current question table");
-        String currentQuestionId = getCurrentQuestionId(dynamoDB);
-        QuerySpec spec, spec1;
-        logger.log("Current question is " + currentQuestionId);
+        if (!item1.getString("id").equals("PlaceBetStage")) {
+            return new GatewayResponse("", headers, 200);
+        }
 
-        String playerId = (String) ((LinkedHashMap<String, Object>) input.get("queryStringParameters")).get("playerId");
-        String amount = (String) ((LinkedHashMap<String, Object>) input.get("queryStringParameters")).get("amount");
-        String position = (String) ((LinkedHashMap<String, Object>) input.get("queryStringParameters")).get("position");
+            logger.log("Querying current question table");
+            String currentQuestionId = getCurrentQuestionId(dynamoDB);
+            QuerySpec spec, spec1;
+            logger.log("Current question is " + currentQuestionId);
 
-        String betId = playerId + ":" + currentQuestionId + ":" + position;
-        spec = new QuerySpec()
-                .withKeyConditionExpression("id = :v_id")
-                .withValueMap(new ValueMap()
-                        .withString(":v_id", betId));
-        String betsTableName = System.getenv("BETS_TABLE_NAME");
-        Table betsTable = dynamoDB.getTable(betsTableName);
-        ItemCollection<QueryOutcome> bet = betsTable.query(spec);
-        String output = "";
+            String playerId = (String) ((LinkedHashMap<String, Object>) input.get("queryStringParameters")).get("playerId");
+            String amount = (String) ((LinkedHashMap<String, Object>) input.get("queryStringParameters")).get("amount");
+            String position = (String) ((LinkedHashMap<String, Object>) input.get("queryStringParameters")).get("position");
 
-        String PlayerTableName = System.getenv("PLAYER_TABLE_NAME");
-        Table PlayerTable = dynamoDB.getTable(PlayerTableName);
-        spec1 = new QuerySpec()
-                .withKeyConditionExpression("id = :v_id")
-                .withValueMap(new ValueMap()
-                        .withString(":v_id", playerId));
-        ItemCollection<QueryOutcome> query = PlayerTable.query(spec1);
-        String PlayerMoney = query.iterator().next().getString("money");
-        logger.log("player money is " + PlayerMoney);
-        double Pmoney = Double.parseDouble(PlayerMoney);
+            String betId = playerId + ":" + currentQuestionId + ":" + position;
+            spec = new QuerySpec()
+                    .withKeyConditionExpression("id = :v_id")
+                    .withValueMap(new ValueMap()
+                            .withString(":v_id", betId));
+            String betsTableName = System.getenv("BETS_TABLE_NAME");
+            Table betsTable = dynamoDB.getTable(betsTableName);
+            ItemCollection<QueryOutcome> bet = betsTable.query(spec);
+            String output = "";
+            String PlayerTableName = System.getenv("PLAYER_TABLE_NAME");
+            Table PlayerTable = dynamoDB.getTable(PlayerTableName);
+            spec1 = new QuerySpec()
+                    .withKeyConditionExpression("id = :v_id")
+                    .withValueMap(new ValueMap()
+                            .withString(":v_id", playerId));
+            ItemCollection<QueryOutcome> query = PlayerTable.query(spec1);
+            String PlayerMoney = query.iterator().next().getString("money");
+            logger.log("player money is " + PlayerMoney);
+            double Pmoney = Double.parseDouble(PlayerMoney);
 
-        Integer amounty = Integer.parseUnsignedInt(amount);
+            Integer amounty = Integer.parseUnsignedInt(amount);
 
 
-        if(amounty <= Pmoney) {
-            if (bet.iterator().hasNext()) {
-                betsTable.updateItem(new PrimaryKey("id", betId), new AttributeUpdate("amount").put(new BigDecimal(amount)));
-                double UpdateMoney = Pmoney - amounty;
-                PlayerTable.updateItem(new PrimaryKey("id", playerId), new AttributeUpdate("money").put(new BigDecimal(UpdateMoney)));
-                output = betsTable.getItem(new PrimaryKey("id", betId)).toJSONPretty();
+            if (amounty <= Pmoney) {
+                if (bet.iterator().hasNext()) {
+                    betsTable.updateItem(new PrimaryKey("id", betId), new AttributeUpdate("amount").put(new BigDecimal(amount)));
+                    double UpdateMoney = Pmoney - amounty;
+                    PlayerTable.updateItem(new PrimaryKey("id", playerId), new AttributeUpdate("money").put(new BigDecimal(UpdateMoney)));
+                    output = betsTable.getItem(new PrimaryKey("id", betId)).toJSONPretty();
+                }
             } else {
                 ScanSpec scanSpec = new ScanSpec()
                         .withFilterExpression("contains(id, :v_playerId)")
@@ -91,10 +90,12 @@ public class PlaceBet implements RequestHandler<LinkedHashMap<String, Object>, G
                     output = newBet.toJSONPretty();
                 }
             }
+
+
+            logger.log(output);
+            return new GatewayResponse(output, headers, 200);
         }
-        logger.log(output);
-        return new GatewayResponse(output, headers, 200);
-    }
+
 
     private String getCurrentQuestionId(DynamoDB dynamoDB) {
         String currentQuestionTableName = System.getenv("CURRENT_QUESTION_TABLE_NAME");
